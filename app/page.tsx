9c7015ad350d387f0c6a58f80de96384f365d1ee
AFRID SHAIK <afridayan01@gmail.com>
2023-10-07 09:27:34 +0000

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <NavBar/>      
     <div >
       <div className="flex text-white flex-col items-start bg-primary 
-        h-1/2  w-full p-4 ">
+        h-1/2  w-full p-4  ">
         <h1 className=" font-ro  text-2xl ">Infolib Technology offers <br/>the best </h1>
         <p className=" text-xs pt-4 pl-4">Cyber security,network security and End to End It services</p>
         <button className="flex items-start  h-fit w-fit px-6 py-2 text-md
@@ -18,11 +18,32 @@ export default function Home() {
             <img className="relative left-10 top-8 animate-pulse  w-6 flex" 
             src="sliderHome.png"
             />
-            
           </div>
         <img src="home1.png" className="w-3/5 justify-center "/>
         </div>
       </div>
+      <div>
+        <div className="flex justify-center items-center mt-10">
+          <label className="bg-blue-50 px-6 py-1 rounded-md text-blue-800
+           text-md
+          ">Our Focus</label>
+        </div>
+        <div className="flex flex-col justify-center items-center p-4 w-full">
+          <label
+          className="text-2xl text-green-800 ">Optimizing your IT infrastructure for maximum performance</label>
+          <embed src="home3.webp" className="w-full p-4 mt-4  object-fill"/>
+          <p className="text-sm p-4 font-light ">INFOLIB is a renowned offshore development services provided commited to 
+            helping organizations accomplish their objectives by granting them access to skilled and cost-effective 
+            resources that can improve their efficiency.
+          </p>
+          <p className="text-sm pl-4 pr-4 font-light pb-4">We Recognize the difficulties that businesses encounter when searching for free</p>
+          <p className="text-sm pl-4 pr-4 font-light">appropriate talent, which is why we provide an all-inclusive solution that incorporates a pool
+            of proficient developers ,UI & UX designers,customer support and technical staff,along
+            with a streamlined proccess management approach.
+          </p>
+        </div>
+      </div>
+      
     </div>
     </>
   )
